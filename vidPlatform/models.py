225589dbcb806d83caeda2a008e66b49563ff649
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-08-22 11:49:52 +0200

    yeeet

--- vidPlatform/models.py
+++ vidPlatform/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class dateEntry(models.Model):
+    date = models.DateTimeField()
+    videoLink = models.CharField(max_length=340)
+    
+    
--- vidPlatform/views.py
+++ vidPlatform/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template("vidPlatform/index.html")
+    context={
+        "bruh":"yeet"
+	}
+    return render(request, "vidPlatform/index.html", context)
+
+
+def detail(request):
+    template = loader.get_template("vidPlatform/detail.html")
+    context={
+        "bruh":"yeet"
+	}
+    return render(request, "vidPlatform/detail.html", context)
