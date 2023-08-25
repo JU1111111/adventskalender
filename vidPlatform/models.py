524812fff8cd726970274661eed9bf3f19012f53
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-08-25 13:00:12 +0200

    hgfjjh

--- vidPlatform/models.py
+++ vidPlatform/models.py
@@ -5,5 +5,10 @@ from django.db import models
 class dateEntry(models.Model):
     date = models.DateTimeField()
     videoLink = models.CharField(max_length=340)
+
+class choice(models.Model):
+    question = models.ForeignKey(dateEntry, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
     
     
