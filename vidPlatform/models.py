31fdc30da4a14cfc24ad423d02b2eae71570dadf
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-08-25 13:16:57 +0200

    sdadasf

--- vidPlatform/models.py
+++ vidPlatform/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class dateEntry(models.Model):
+class DateEntry(models.Model):
     date = models.DateTimeField()
     videoLink = models.CharField(max_length=340)
 
 class choice(models.Model):
-    question = models.ForeignKey(dateEntry, on_delete=models.CASCADE)
+    question = models.ForeignKey(DateEntry, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     
--- vidPlatform/views.py
+++ vidPlatform/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import DateEntry
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 
 # Create your views here.
 
 def index(request):
+    latest_list = DateEntry.objects.order_by("-date")[:24]
     template = loader.get_template("vidPlatform/index.html")
-    context={
-        "bruh":"yeet"
-	}
-    return render(request, "vidPlatform/index.html", context)
+    context = {
+        "latest_list": latest_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
-def detail(request):
-    template = loader.get_template("vidPlatform/detail.html")
-    context={
-        "bruh":"yeet"
-	}
-    return render(request, "vidPlatform/detail.html", context)
+
+
+def detail(request, dateentry_id):
+    try:
+        dateEntry = get_object_or_404(DateEntry, pk=dateentry_id)
+    except DateEntry.DoesNotExist:
+        raise Http404("date entry doesnt exist")
+    return render(request,"vidPlatform/detail.html", {"entry":dateEntry} )
