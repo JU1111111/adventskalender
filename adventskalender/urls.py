02790d1713717f7f92fc377a941376b9804e1268
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-09-08 13:08:45 +0200

    userreg app

--- adventskalender/urls.py
+++ adventskalender/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('advent/', include("vidPlatform.urls")),
+	path('userReg/',include("userReg.urls"))
 ]
---
+++ userReg/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+# Create your views here.
+
+def index(req):
+	return HttpResponse("User Registration Form here")
