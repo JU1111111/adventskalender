b78ffbdde1355d94b4d8565dee19aa08601c7c24
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-09-12 21:09:39 +0200

    gbdfsjklöväx

--- adventskalender/urls.py
+++ adventskalender/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('advent/', include("vidPlatform.urls")),
-	path('userReg/',include("userReg.urls"))
+	path('register/',include("userReg.urls"))
 ]
--- userReg/urls.py
+++ userReg/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.register_request, name="register"),
+    path("", views.register_request, name="register_request"),
+	path("login", views.login_request, name="login")
 ]
