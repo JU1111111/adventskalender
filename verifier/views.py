bff53f2dcf4d3307009859e384f65bd9440bb7cc
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-11-17 14:48:10 +0100

    yeeeeeeeeeeeeet

--- verifier/views.py
+++ verifier/views.py
@@ -6,6 +6,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from adminDash.funtions.emailTesters import getDataFromTheJson
 
 from.forms import NewStudentForm, NewUserForm
 from .tokens import account_activation_token
@@ -65,7 +66,8 @@ def activateEmail(request, user, to_email):
 		'token': account_activation_token.make_token(user),
 		'protocol': 'https' if request.is_secure() else 'http'
 	})
-	email = EmailMessage(mail_subject, message, to=[to_email])
+	fromMail = getDataFromTheJson()['fromMailAddr']
+	email = EmailMessage(mail_subject, message, to=[to_email], from_email=fromMail)
 	if email.send():
 		messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
 			received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
