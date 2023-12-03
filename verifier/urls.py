7ce10fc5b9f198c509043cd773cb5bf7e5290f18
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-12-03 22:23:17 +0100

    custom pw reset pages

--- verifier/urls.py
+++ verifier/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include 
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
 	path('', views.register, name='register'),
+	path(
+        "accounts/password_reset/",
+        auth_views.PasswordResetView.as_view(template_name="verifier/templates/registration/password_reset_form.html"),
+    ),
+	path(
+        "accounts/password_reset/done/",
+        auth_views.PasswordResetView.as_view(template_name="verifier/templates/registration/password_reset_done.html"),
+    ),
 	path('accounts/', include("django.contrib.auth.urls"), name='register'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
 	path('NotActive', views.notActive, name='notActivate'),
