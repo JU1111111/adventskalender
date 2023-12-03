052e6751e7033d67a3a5b45d2e1a46f5e6ae27e5
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-12-03 22:35:36 +0100

    confirm page redesign

--- verifier/urls.py
+++ verifier/urls.py
@@ -11,9 +11,14 @@ urlpatterns = [
     ),
 	path(
         "accounts/password_reset/done/",
-        auth_views.PasswordResetView.as_view(template_name="verifier/templates/registration/password_reset_done.html"),
+        auth_views.PasswordResetDoneView.as_view(template_name="verifier/templates/registration/password_reset_done.html"),
+    ),
+	path(
+        "/accounts/reset/",
+        auth_views.PasswordResetConfirmView.as_view(template_name="verifier/templates/registration/password_reset_done.html"),
     ),
 	path('accounts/', include("django.contrib.auth.urls"), name='register'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
 	path('NotActive', views.notActive, name='notActivate'),
+	
 ]
