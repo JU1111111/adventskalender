fd80a2c4c40b2babde96770f665cc67084081722
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-12-03 23:00:07 +0100

    outcomented stuff

--- verifier/urls.py
+++ verifier/urls.py
@@ -5,19 +5,19 @@ from . import views
 
 urlpatterns = [
 	path('', views.register, name='register'),
-	path(
+	'''path(
         "accounts/password_reset/",
         auth_views.PasswordResetView.as_view(template_name="verifier/templates/registration/password_reset_form.html"),
     ),
 	path(
         "accounts/password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(template_name="verifier/templates/registration/password_reset_done.html"),
-    ),
-	path(
+    ),'''
+	'''path(
         "/accounts/reset/",
         auth_views.PasswordResetConfirmView.as_view(template_name="verifier/templates/registration/password_reset_confirm.html"),
     	name='password_reset_confirm',
-		),
+		),'''
 	path('accounts/', include("django.contrib.auth.urls"), name='register'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
 	path('NotActive', views.notActive, name='notActivate'),
