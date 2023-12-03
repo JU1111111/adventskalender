363fb8ff6a7a23fe830e2fef9368a25c5e76fcd3
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-12-03 21:07:43 +0100

    pw reset

--- verifier/urls.py
+++ verifier/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include 
 from . import views
 
 urlpatterns = [
 	path('', views.register, name='register'),
+	path('accounts/', include("django.contrib.auth.urls"), name='register'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
 	path('NotActive', views.notActive, name='notActivate'),
 ]
