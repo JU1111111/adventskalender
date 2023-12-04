46ed9aa7f9abb7e789fd2ea8fd3590e74a3a6c40
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-12-04 23:47:41 +0100

    proper admin user usage

--- adminDash/funtions/dbMod.py
+++ adminDash/funtions/dbMod.py
@@ -163,10 +163,13 @@ def getCurrentWinners(years:[], forceRefresh=False):
 
 
 def getUserPlacement(username):
-	user = UserModel.objects.get(username=username) 
-	studentOfUser = Student.objects.get(user=user)
-	if not studentOfUser:
+	user = UserModel.objects.get(username=username)
+
+	try:
+		studentOfUser = Student.objects.get(user=user)
+	except:
 		return "None"
+
 	if studentOfUser.studentYear in range(5,9):
 		years = range(5,9)
 	elif studentOfUser.studentYear in range(9,13):
